4b7b594c9baf3b6db491d0463836f527db7050d0
Sammy <andrehplayah@gmail.com>
2020-11-12 22:47:15 +0100

    GUI.h - Better code styling

--- Frontend/GUI/gui.h
+++ Frontend/GUI/gui.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "../util.h"
 
-struct NeoniteGUI {
+struct NeoniteGUI
+{
 	char InputBuf[256];
 	ImVector<char*> Items;
 	ImVector<const char*> Commands;
@@ -11,7 +12,8 @@ struct NeoniteGUI {
 	bool AutoScroll;
 	bool ScrollToBottom;
 
-	NeoniteGUI() {
+	NeoniteGUI()
+	{
 		ClearLog();
 		memset(InputBuf, 0, sizeof(InputBuf));
 		HistoryPos = -1;
@@ -25,25 +27,30 @@ struct NeoniteGUI {
 		ScrollToBottom = false;
 	}
 
-	~NeoniteGUI() {
+	~NeoniteGUI()
+	{
 		ClearLog();
 		for (int i = 0; i < History.Size; i++)
 			free(History[i]);
 	}
 
 	// Portable helpers
-	static int Stricmp(const char* s1, const char* s2) {
+	static int Stricmp(const char* s1, const char* s2)
+	{
 		int d;
-		while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
+		while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1)
+		{
 			s1++;
 			s2++;
 		}
 		return d;
 	}
 
-	static int Strnicmp(const char* s1, const char* s2, int n) {
+	static int Strnicmp(const char* s1, const char* s2, int n)
+	{
 		int d = 0;
-		while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
+		while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1)
+		{
 			s1++;
 			s2++;
 			n--;
@@ -51,21 +58,24 @@ struct NeoniteGUI {
 		return d;
 	}
 
-	static char* Strdup(const char* s) {
+	static char* Strdup(const char* s)
+	{
 		size_t len = strlen(s) + 1;
 		void* buf = malloc(len);
 		IM_ASSERT(buf);
-		return (char*)memcpy(buf, (const void*)s, len);
+		return static_cast<char*>(memcpy(buf, static_cast<const void*>(s), len));
 	}
 
-	static void Strtrim(char* s) {
+	static void Strtrim(char* s)
+	{
 		char* str_end = s + strlen(s);
 		while (str_end > s && str_end[-1] == ' ')
 			str_end--;
 		*str_end = 0;
 	}
 
-	void ClearLog() {
+	void ClearLog()
+	{
 		for (int i = 0; i < Items.Size; i++)
 			free(Items[i]);
 		Items.clear();
@@ -83,7 +93,8 @@ struct NeoniteGUI {
 		Items.push_back(Strdup(buf));
 	}
 
-	void Draw(const char* title, bool* p_open) {
+	void Draw(const char* title, bool* p_open)
+	{
 		/*ImGui::SetNextWindowSize(ImVec2(520, 600), ImGuiCond_FirstUseEver);
 		if (!ImGui::Begin(title, p_open))
 		{
@@ -115,16 +126,18 @@ struct NeoniteGUI {
 		const float footer_height_to_reserve =
 			ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
 		ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve),
-			false, ImGuiWindowFlags_HorizontalScrollbar);
+		                  false, ImGuiWindowFlags_HorizontalScrollbar);
 
-		if (ImGui::BeginPopupContextWindow()) {
+		if (ImGui::BeginPopupContextWindow())
+		{
 			if (ImGui::Selectable("Clear"))
 				ClearLog();
 		}
 
 		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1));
 		// Tighten spacing
-		for (int i = 0; i < Items.Size; i++) {
+		for (int i = 0; i < Items.Size; i++)
+		{
 			const char* item = Items[i];
 			if (!Filter.PassFilter(item))
 				continue;
@@ -133,19 +146,23 @@ struct NeoniteGUI {
 			// (e.g. make Items[] an array of structure, store color/type etc.)
 			ImVec4 color;
 			bool has_color = false;
-			if (strstr(item, "[+]")) {
+			if (strstr(item, "[+]"))
+			{
 				color = ImColor(0, 255, 50);
 				has_color = true;
 			}
-			else if (strstr(item, "[x]")) {
+			else if (strstr(item, "[x]"))
+			{
 				color = ImColor(255, 0, 0);
 				has_color = true;
 			}
-			else if (strstr(item, "[=]")) {
+			else if (strstr(item, "[=]"))
+			{
 				color = ImColor(85, 0, 255);
 				has_color = true;
 			}
-			else if (strstr(item, "[>]")) {
+			else if (strstr(item, "[>]"))
+			{
 				color = ImColor(135, 25, 195);
 				has_color = true;
 			}
@@ -188,17 +205,19 @@ struct NeoniteGUI {
 		if (reclaim_focus)
 			ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
 
-		  //ImGui::End();
+		//ImGui::End();
 	}
 
-	void ExecCommand(const char* command_line) {
+	void ExecCommand(const char* command_line)
+	{
 		AddLog("# %s\n", command_line);
 
 		// Insert into history. First find match and delete it so it can be pushed to the back.
 		// This isn't trying to be smart or optimal.
 		HistoryPos = -1;
 		for (int i = History.Size - 1; i >= 0; i--)
-			if (Stricmp(History[i], command_line) == 0) {
+			if (Stricmp(History[i], command_line) == 0)
+			{
 				free(History[i]);
 				History.erase(History.begin() + i);
 				break;
@@ -206,20 +225,24 @@ struct NeoniteGUI {
 		History.push_back(Strdup(command_line));
 
 		// Process command
-		if (Stricmp(command_line, "CLEAR") == 0) {
+		if (Stricmp(command_line, "CLEAR") == 0)
+		{
 			ClearLog();
 		}
-		else if (Stricmp(command_line, "HELP") == 0) {
+		else if (Stricmp(command_line, "HELP") == 0)
+		{
 			AddLog("Commands:");
 			for (int i = 0; i < Commands.Size; i++)
 				AddLog("- %s", Commands[i]);
 		}
-		else if (Stricmp(command_line, "HISTORY") == 0) {
+		else if (Stricmp(command_line, "HISTORY") == 0)
+		{
 			int first = History.Size - 10;
 			for (int i = first > 0 ? first : 0; i < History.Size; i++)
 				AddLog("%3d: %s\n", i, History[i]);
 		}
-		else {
+		else
+		{
 			AddLog("Unknown command: '%s'\n", command_line);
 		}
 
@@ -228,102 +251,114 @@ struct NeoniteGUI {
 	}
 
 	// In C++11 you'd be better off using lambdas for this sort of forwarding callbacks
-	static int TextEditCallbackStub(ImGuiInputTextCallbackData* data) {
-		NeoniteGUI* console = (NeoniteGUI*)data->UserData;
+	static int TextEditCallbackStub(ImGuiInputTextCallbackData* data)
+	{
+		NeoniteGUI* console = static_cast<NeoniteGUI*>(data->UserData);
 		return console->TextEditCallback(data);
 	}
 
-	int TextEditCallback(ImGuiInputTextCallbackData* data) {
+	int TextEditCallback(ImGuiInputTextCallbackData* data)
+	{
 		//AddLog("cursor: %d, selection: %d-%d", data->CursorPos, data->SelectionStart, data->SelectionEnd);
-		switch (data->EventFlag) {
-		case ImGuiInputTextFlags_CallbackCompletion: {
-			// Example of TEXT COMPLETION
+		switch (data->EventFlag)
+		{
+		case ImGuiInputTextFlags_CallbackCompletion:
+			{
+				// Example of TEXT COMPLETION
 
-			// Locate beginning of current word
-			const char* word_end = data->Buf + data->CursorPos;
-			const char* word_start = word_end;
-			while (word_start > data->Buf) {
-				const char c = word_start[-1];
-				if (c == ' ' || c == '\t' || c == ',' || c == ';')
-					break;
-				word_start--;
-			}
-
-			// Build a list of candidates
-			ImVector<const char*> candidates;
-			for (int i = 0; i < Commands.Size; i++)
-				if (Strnicmp(Commands[i], word_start, (int)(word_end - word_start)) == 0
-					)
-					candidates.push_back(Commands[i]);
-
-			if (candidates.Size == 0) {
-				// No match
-				AddLog("No match for \"%.*s\"!\n", (int)(word_end - word_start),
-					word_start);
-			}
-			else if (candidates.Size == 1) {
-				// Single match. Delete the beginning of the word and replace it entirely so we've got nice casing.
-				data->DeleteChars((int)(word_start - data->Buf),
-					(int)(word_end - word_start));
-				data->InsertChars(data->CursorPos, candidates[0]);
-				data->InsertChars(data->CursorPos, " ");
-			}
-			else {
-				// Multiple matches. Complete as much as we can..
-				// So inputing "C"+Tab will complete to "CL" then display "CLEAR" and "CLASSIFY" as matches.
-				int match_len = (int)(word_end - word_start);
-				for (;;) {
-					int c = 0;
-					bool all_candidates_matches = true;
-					for (int i = 0; i < candidates.Size && all_candidates_matches; i++)
-						if (i == 0)
-							c = toupper(candidates[i][match_len]);
-						else
-							if (c == 0 || c != toupper(candidates[i][match_len]))
-								all_candidates_matches = false;
-					if (!all_candidates_matches)
+				// Locate beginning of current word
+				const char* word_end = data->Buf + data->CursorPos;
+				const char* word_start = word_end;
+				while (word_start > data->Buf)
+				{
+					const char c = word_start[-1];
+					if (c == ' ' || c == '\t' || c == ',' || c == ';')
 						break;
-					match_len++;
+					word_start--;
 				}
 
-				if (match_len > 0) {
-					data->DeleteChars((int)(word_start - data->Buf),
-						(int)(word_end - word_start));
-					data->InsertChars(data->CursorPos, candidates[0],
-						candidates[0] + match_len);
+				// Build a list of candidates
+				ImVector<const char*> candidates;
+				for (int i = 0; i < Commands.Size; i++)
+					if (Strnicmp(Commands[i], word_start, static_cast<int>(word_end - word_start)) == 0
+					)
+						candidates.push_back(Commands[i]);
+
+				if (candidates.Size == 0)
+				{
+					// No match
+					AddLog("No match for \"%.*s\"!\n", static_cast<int>(word_end - word_start),
+					       word_start);
 				}
-
-				// List matches
-				AddLog("Possible matches:\n");
-				for (int i = 0; i < candidates.Size; i++)
-					AddLog("- %s\n", candidates[i]);
-			}
-
-			break;
-		}
-		case ImGuiInputTextFlags_CallbackHistory: {
-			// Example of HISTORY
-			const int prev_history_pos = HistoryPos;
-			if (data->EventKey == ImGuiKey_UpArrow) {
-				if (HistoryPos == -1)
-					HistoryPos = History.Size - 1;
+				else if (candidates.Size == 1)
+				{
+					// Single match. Delete the beginning of the word and replace it entirely so we've got nice casing.
+					data->DeleteChars(static_cast<int>(word_start - data->Buf),
+					                  static_cast<int>(word_end - word_start));
+					data->InsertChars(data->CursorPos, candidates[0]);
+					data->InsertChars(data->CursorPos, " ");
+				}
 				else
-					if (HistoryPos > 0)
-						HistoryPos--;
-			}
-			else if (data->EventKey == ImGuiKey_DownArrow) {
-				if (HistoryPos != -1)
-					if (++HistoryPos >= History.Size)
-						HistoryPos = -1;
-			}
+				{
+					// Multiple matches. Complete as much as we can..
+					// So inputing "C"+Tab will complete to "CL" then display "CLEAR" and "CLASSIFY" as matches.
+					int match_len = static_cast<int>(word_end - word_start);
+					for (;;)
+					{
+						int c = 0;
+						bool all_candidates_matches = true;
+						for (int i = 0; i < candidates.Size && all_candidates_matches; i++)
+							if (i == 0)
+								c = toupper(candidates[i][match_len]);
+							else if (c == 0 || c != toupper(candidates[i][match_len]))
+								all_candidates_matches = false;
+						if (!all_candidates_matches)
+							break;
+						match_len++;
+					}
 
-			// A better implementation would preserve the data on the current input line along with cursor position.
-			if (prev_history_pos != HistoryPos) {
-				const char* history_str = (HistoryPos >= 0) ? History[HistoryPos] : "";
-				data->DeleteChars(0, data->BufTextLen);
-				data->InsertChars(0, history_str);
+					if (match_len > 0)
+					{
+						data->DeleteChars(static_cast<int>(word_start - data->Buf),
+						                  static_cast<int>(word_end - word_start));
+						data->InsertChars(data->CursorPos, candidates[0],
+						                  candidates[0] + match_len);
+					}
+
+					// List matches
+					AddLog("Possible matches:\n");
+					for (int i = 0; i < candidates.Size; i++)
+						AddLog("- %s\n", candidates[i]);
+				}
+
+				break;
 			}
-		}
+		case ImGuiInputTextFlags_CallbackHistory:
+			{
+				// Example of HISTORY
+				const int prev_history_pos = HistoryPos;
+				if (data->EventKey == ImGuiKey_UpArrow)
+				{
+					if (HistoryPos == -1)
+						HistoryPos = History.Size - 1;
+					else if (HistoryPos > 0)
+						HistoryPos--;
+				}
+				else if (data->EventKey == ImGuiKey_DownArrow)
+				{
+					if (HistoryPos != -1)
+						if (++HistoryPos >= History.Size)
+							HistoryPos = -1;
+				}
+
+				// A better implementation would preserve the data on the current input line along with cursor position.
+				if (prev_history_pos != HistoryPos)
+				{
+					const char* history_str = (HistoryPos >= 0) ? History[HistoryPos] : "";
+					data->DeleteChars(0, data->BufTextLen);
+					data->InsertChars(0, history_str);
+				}
+			}
 		}
 		return 0;
 	}
