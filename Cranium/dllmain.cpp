9bf4a9bd0aef93de1b0a9e8a0b6b63e050aca1b3
Sammy <andrehplayah@gmail.com>
2020-12-19 13:32:49 +0100

    add init minhook and check if it worked

--- Cranium/dllmain.cpp
+++ Cranium/dllmain.cpp
@@ -16,68 +16,74 @@ void dllMain()
 	freopen_s(&fDummy, "CONOUT$", "w", stdout);
 #endif
 
-	Hooks::init();
+	MH_STATUS initMH = MH_Initialize();
 
-	//CURL Detour
+	if (initMH = MH_OK) {
+		Hooks::init();
+
+		//CURL Detour
 #ifdef SSL_BYPASS
 
-	CurlEasySetOpt = decltype(CurlEasySetOpt)(CurlEasyAdd);
-	CurlSetOpt = decltype(CurlSetOpt)(CurlSetAdd);
+		CurlEasySetOpt = decltype(CurlEasySetOpt)(CurlEasyAdd);
+		CurlSetOpt = decltype(CurlSetOpt)(CurlSetAdd);
 
-	VEH::EnableHook(CurlEasySetOpt, CurlEasySetOptDetour);
+		VEH::EnableHook(CurlEasySetOpt, CurlEasySetOptDetour);
 
 #endif
 
-	bool isUnlocked = false;
-	bool isHooked = false;
-	while (true)
-	{
-		//Process Event Hooking
-#ifdef PROCESS_EVENT
-		if (GetAsyncKeyState(VK_F10) && !isHooked)
+		bool isUnlocked = false;
+		bool isHooked = false;
+		while (true)
 		{
-			ProcessEvent = decltype(ProcessEvent)(ProcessEventAdd);
+			//Process Event Hooking
+#ifdef PROCESS_EVENT
+			if (GetAsyncKeyState(VK_F10) && !isHooked)
+			{
+				ProcessEvent = decltype(ProcessEvent)(ProcessEventAdd);
 
-			//VEH::EnableHook(ProcessEvent, ProcessEventDetour);
+				//VEH::EnableHook(ProcessEvent, ProcessEventDetour);
 
-			MH_CreateHook((LPVOID)ProcessEventAdd, ProcessEventDetour, (LPVOID*)ProcessEvent); //lpvoid should work
-            MH_EnableHook((LPVOID)ProcessEventAdd); //LPVoid should work
-			isHooked = true;
-			printf("\n[+] ProcessEvent was hooked!\n");
-		}
+				MH_CreateHook((LPVOID)ProcessEventAdd, ProcessEventDetour, (LPVOID*)ProcessEvent); //lpvoid should work
+				MH_EnableHook((LPVOID)ProcessEventAdd); //LPVoid should work
+				isHooked = true;
+				printf("\n[+] ProcessEvent was hooked!\n");
+			}
 #endif
 
 #ifdef UNLOCK_CONSOLE
-		//Console Spawner
-		if (GetAsyncKeyState(VK_OEM_3) && !isUnlocked)
-		{
-			GEngine = *(UEngine**)(GEngineAdd + 22 + *(int32_t*)(GEngineAdd + 18));
+			//Console Spawner
+			if (GetAsyncKeyState(VK_OEM_3) && !isUnlocked)
+			{
+				GEngine = *(UEngine**)(GEngineAdd + 22 + *(int32_t*)(GEngineAdd + 18));
 
-			StaticConstructObject_Internal = (f_StaticConstructObject_Internal)(SCOIAdd);
+				StaticConstructObject_Internal = (f_StaticConstructObject_Internal)(SCOIAdd);
 
-			UConsole* Console = reinterpret_cast<UConsole*>(StaticConstructObject_Internal(
-				GEngine->ConsoleClass,
-				reinterpret_cast<UObject*>(GEngine->GameViewportClient),
-				nullptr,
-				RF_NoFlags,
-				None,
-				nullptr,
-				false,
-				nullptr,
-				false
-			));
+				UConsole* Console = reinterpret_cast<UConsole*>(StaticConstructObject_Internal(
+					GEngine->ConsoleClass,
+					reinterpret_cast<UObject*>(GEngine->GameViewportClient),
+					nullptr,
+					RF_NoFlags,
+					None,
+					nullptr,
+					false,
+					nullptr,
+					false
+				));
 
-			GEngine->GameViewportClient->ViewportConsole = Console;
-			isUnlocked = true;
-			printf("\n[+]Console was unlocked!\n");
-		}
+				GEngine->GameViewportClient->ViewportConsole = Console;
+				isUnlocked = true;
+				printf("\n[+]Console was unlocked!\n");
+			}
 #endif
 
 #if(defined UNLOCK_CONSOLE && defined PROCESS_EVENT)
-		if (isUnlocked && isHooked) break;
+			if (isUnlocked && isHooked) break;
 #endif
 
-		Sleep(300);
+			Sleep(300);
+		}
+	} else {
+		printf("\n[+] Failure initializing Minhook!");
 	}
 }
 
