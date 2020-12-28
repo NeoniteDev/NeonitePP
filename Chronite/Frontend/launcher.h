b7bacdf6f315ed69ceaec4355a6889dbb968f05c
Sammy <andrehplayah@gmail.com>
2020-12-28 22:45:25 +0100

    Heartbeat start code - kemo l8r we try to implement check x secs thx

--- Chronite/Frontend/launcher.h
+++ Chronite/Frontend/launcher.h
@@ -9,6 +9,23 @@ inline HANDLE hClient = INVALID_HANDLE_VALUE;
 
 namespace launcher
 {
+	inline bool isFortniteRunning(const wchar_t* processName)
+	{
+		bool exists = false;
+		PROCESSENTRY32 entry;
+		entry.dwSize = sizeof(PROCESSENTRY32);
+
+		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+		if (Process32First(snapshot, &entry))
+			while (Process32Next(snapshot, &entry))
+				if (!wcsicmp((wchar_t*)entry.szExeFile, processName))
+					exists = true;
+
+		CloseHandle(snapshot);
+		return exists;
+	}
+	
 	inline void init()
 	{
 		char programData[MAX_PATH];
