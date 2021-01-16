aee611130d905573dfad3a5d41960b7a1ac904d8
Sammy <andrehplayah@gmail.com>
2021-01-16 23:24:52 +0100

    Starting to group the patterns for not messy code........ Starting to implement 7.30

--- Cranium/hooks.h
+++ Cranium/hooks.h
@@ -15,10 +15,10 @@ namespace Hooks
 	{
 		//cURL Hooking.
 		//Works on all versions
-		const auto CurlEasyAdd = Util::FindPattern(Patterns::CurlEasySetOpt, Masks::CurlEasySetOpt);
+		const auto CurlEasyAdd = Util::FindPattern(Patterns::Curl::CurlEasySetOpt, Masks::Curl::CurlEasySetOpt);
 		VALIDATE_ADDRESS(CurlEasyAdd, XOR("Failed to find Curl Easy Address."));
 
-		const auto CurlSetAdd = Util::FindPattern(Patterns::CurlSetOpt, Masks::CurlSetOpt);
+		const auto CurlSetAdd = Util::FindPattern(Patterns::Curl::CurlSetOpt, Masks::Curl::CurlSetOpt);
 		VALIDATE_ADDRESS(CurlSetAdd, XOR("Failed to find Curl SetOpt Address."));
 
 		CurlEasySetOpt = decltype(CurlEasySetOpt)(CurlEasyAdd);
@@ -40,21 +40,21 @@ namespace Hooks
 
 		//Used for ProcessEvent Hooking.
 		//Tested from 12.41 to latest
-		const auto ProcessEventAdd = Util::FindPattern(Patterns::ProcessEvent, Masks::ProcessEvent);
+		const auto ProcessEventAdd = Util::FindPattern(Patterns::bLatest::ProcessEvent, Masks::bLatest::ProcessEvent);
 		VALIDATE_ADDRESS(ProcessEventAdd, XOR("Failed to find ProcessEvent Address."));
 
 		ProcessEvent = decltype(ProcessEvent)(ProcessEventAdd);
 
 		//Used for Camera Hooking.
 		//Tested from 12.41 to latest
-		const auto GetViewPointAdd = Util::FindPattern(Patterns::GetViewPoint, Masks::GetViewPoint);
+		const auto GetViewPointAdd = Util::FindPattern(Patterns::bLatest::GetViewPoint, Masks::bLatest::GetViewPoint);
 		VALIDATE_ADDRESS(GetViewPointAdd, XOR("Failed to find GetViewPoint Address."));
 
 		GetViewPoint = decltype(GetViewPoint)(GetViewPointAdd);
 
 		//Used for getting UObjects names.
 		//Tested from 12.41 to latest
-		const auto GONIAdd = Util::FindPattern(Patterns::GONI, Masks::GONI);
+		const auto GONIAdd = Util::FindPattern(Patterns::bLatest::GONI, Masks::bLatest::GONI);
 		VALIDATE_ADDRESS(GONIAdd, XOR("Failed to find GetObjectName Address."));
 
 		GetObjectNameInternal = decltype(GetObjectNameInternal)(GONIAdd);
@@ -62,7 +62,7 @@ namespace Hooks
 		//Used for getting UObjects full names.
 		if (gVersion == "12.41")
 		{
-			const auto GetObjectFullNameAdd = Util::FindPattern(Patterns::GetObjectFullName12_41, Masks::GetObjectFullName12_41);
+			const auto GetObjectFullNameAdd = Util::FindPattern(Patterns::bTravis::GetObjectFullName, Masks::bTravis::GetObjectFullName);
 			VALIDATE_ADDRESS(GetObjectFullNameAdd, XOR("Failed to find GetObjectFullName Address."));
 
 			GetObjectFullNameInternal = decltype(GetObjectFullNameInternal)(GetObjectFullNameAdd);
@@ -70,14 +70,14 @@ namespace Hooks
 		else
 		{
 			//14.30^
-			const auto GetObjectFullNameAdd = Util::FindPattern(Patterns::GetObjectFullName, Masks::GetObjectFullName);
+			const auto GetObjectFullNameAdd = Util::FindPattern(Patterns::bLatest::GetObjectFullName, Masks::bLatest::GetObjectFullName);
 			VALIDATE_ADDRESS(GetObjectFullNameAdd, XOR("Failed to find GetObjectFullName Address."));
 
 			GetObjectFullNameInternal = decltype(GetObjectFullNameInternal)(GetObjectFullNameAdd);
 		}
 
 		//Used for getting FFields full names.
-		const auto GetFullNameAdd = Util::FindPattern(Patterns::GetFullName, Masks::GetFullName);
+		const auto GetFullNameAdd = Util::FindPattern(Patterns::bLatest::GetFullName, Masks::bLatest::GetFullName);
 		VALIDATE_ADDRESS(GetFullNameAdd, XOR("Failed to find GetFullName Address."));
 
 		GetFullName = decltype(GetFullName)(GetFullNameAdd);
@@ -85,7 +85,7 @@ namespace Hooks
 
 		//Used to construct objects, mostly used for console stuff.
 		//Tested from 12.41 to latest
-		const auto SCOIAdd = Util::FindPattern(Patterns::SCOI, Masks::SCOI);
+		const auto SCOIAdd = Util::FindPattern(Patterns::bLatest::SCOI, Masks::bLatest::SCOI);
 		VALIDATE_ADDRESS(SCOIAdd, XOR("Failed to find SCOI Address."));
 
 		StaticConstructObject = decltype(StaticConstructObject)(SCOIAdd);
@@ -94,7 +94,7 @@ namespace Hooks
 		//Used for mostly everything.
 		if (gVersion == "12.41")
 		{
-			const auto GEngineAdd = Util::FindPattern(Patterns::GEngine12_41, Masks::GEngine12_41);
+			const auto GEngineAdd = Util::FindPattern(Patterns::bTravis::GEngine, Masks::bTravis::GEngine);
 
 			VALIDATE_ADDRESS(GEngineAdd, XOR("Failed to find GEngine Address."));
 
@@ -103,7 +103,7 @@ namespace Hooks
 		else
 		{
 			//14.30^
-			const auto GEngineAdd = Util::FindPattern(Patterns::GEngine, Masks::GEngine);
+			const auto GEngineAdd = Util::FindPattern(Patterns::bLatest::GEngine, Masks::bLatest::GEngine);
 			VALIDATE_ADDRESS(GEngineAdd, XOR("Failed to find GEngine Address."));
 
 			GEngine = *reinterpret_cast<UEngine**>(GEngineAdd + 22 + *reinterpret_cast<int32_t*>(GEngineAdd + 18));
@@ -112,14 +112,14 @@ namespace Hooks
 		//Used to find objects, dump them, mostly works as an alternative for the ObjectFinder.
 		if (gVersion == "12.41")
 		{
-			const auto GObjectsAdd = Util::FindPattern(Patterns::GObjects12_41, Masks::GObjects12_41);
+			const auto GObjectsAdd = Util::FindPattern(Patterns::bTravis::GObjects, Masks::bTravis::GObjects);
 			VALIDATE_ADDRESS(GObjectsAdd, XOR("Failed to find GObjects Address."));
 			GObjs = decltype(GObjs)(RELATIVE_ADDRESS(GObjectsAdd, 7));
 		}
 		else
 		{
 			//14.30^
-			const auto GObjectsAdd = Util::FindPattern(Patterns::GObjects, Masks::GObjects);
+			const auto GObjectsAdd = Util::FindPattern(Patterns::bLatest::GObjects, Masks::bLatest::GObjects);
 			VALIDATE_ADDRESS(GObjectsAdd, XOR("Failed to find GObjects Address."));
 			GObjs = decltype(GObjs)(RELATIVE_ADDRESS(GObjectsAdd, 7));
 		}
